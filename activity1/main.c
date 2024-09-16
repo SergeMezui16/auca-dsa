7e160288d637d3b8f7f9e2c3751e14cc1902cfd5
Serge Mezui <sergemezui.dev@gmail.com>
2024-09-16 09:57:55 +0200

    init the activity one

---
+++ activity1/main.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * Activity One:
+ * 
+ * Print the time table for 8 in one column
+ * 
+ * @author Serge Mezui <sergemezui.dev@gmail.com>
+ */
+int main()
+{
+
+    return 0;
+}
